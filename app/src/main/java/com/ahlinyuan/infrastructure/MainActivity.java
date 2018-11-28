e46c8dc0170638ffee4f77aa66099eed1f169507
ahlinyuan <ahlinyuan@foxmail.com>
2018-11-28 16:31:44 +0800

    all commit

---
+++ app/src/main/java/com/ahlinyuan/infrastructure/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ahlinyuan.infrastructure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
